9c778118ba4bdad2ba6ee8f16985556dbb04be50
MohannadNaj <me@mohannad.io>
2017-06-20 11:47:39 +0300

    Add Parameters Helper

---
+++ app/helpers/parameters.php
@@ -0,0 +1,14 @@
+<?php
+// return parameters collection, or a specified parameter value
+if (! function_exists('param')) {
+    function param($name = false)
+    {
+        // if we are using the method to return a specified parameter value, e.g: param('some_param')
+        if ($name) {
+            return app('parameter')->where('name', $name)->first()->getValue();
+        }
+
+        // if the method is used to return the parameter model instance
+        return app('parameter');
+    }
+}
