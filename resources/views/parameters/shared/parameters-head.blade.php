37e401b72b3c0d923b088c886da555eb37af7b11
MohannadNaj <me@mohannad.io>
2017-06-21 14:07:46 +0300

    Bootstrap Offcanvas Template

---
+++ resources/views/parameters/shared/parameters-head.blade.php
@@ -0,0 +1,82 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{asset('vendor/parameters/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/parameters/css/bootstrap-theme.min.css')}}" rel="stylesheet">
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+            'base_url' => url('/') . '/',
+            'auth_user_id' => auth()->id(),
+        ] ); ?>;
+        @stack('js_head')
+    </script>
+    
+    <style type="text/css">
+/*
+ * Style tweaks
+ * --------------------------------------------------
+ */
+html,
+body {
+  overflow-x: hidden; /* Prevent scroll on narrow devices */
+}
+body {
+  padding-top: 70px;
+}
+footer {
+  padding: 30px 0;
+}
+
+/*
+ * Off Canvas
+ * --------------------------------------------------
+ */
+@media screen and (max-width: 767px) {
+  .row-offcanvas {
+    position: relative;
+    -webkit-transition: all .25s ease-out;
+         -o-transition: all .25s ease-out;
+            transition: all .25s ease-out;
+  }
+
+  .row-offcanvas-right {
+    right: 0;
+  }
+
+  .row-offcanvas-left {
+    left: 0;
+  }
+
+  .row-offcanvas-right
+  .sidebar-offcanvas {
+    right: -50%; /* 6 columns */
+  }
+
+  .row-offcanvas-left
+  .sidebar-offcanvas {
+    left: -50%; /* 6 columns */
+  }
+
+  .row-offcanvas-right.active {
+    right: 50%; /* 6 columns */
+  }
+
+  .row-offcanvas-left.active {
+    left: 50%; /* 6 columns */
+  }
+
+  .sidebar-offcanvas {
+    position: absolute;
+    top: 0;
+    width: 50%; /* 6 columns */
+  }
+}
+    </style>
---
+++ resources/views/parameters/shared/parameters-layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('parameters.shared.parameters-head')
+</head>
+<body>
+    @yield('content')
+@stack('modals')
+{{-- <img src="{{asset('images/loading.gif')}}" class="loading" style="display: none; position: fixed; left: 0; bottom: 0;"> --}}
+<!-- Scripts -->
+<script src="{{asset('vendor/parameters/js/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/parameters/js/bootstrap.min.js')}}"></script>
+<script type="text/javascript">
+    @stack('js_footer')
+    $(document).ready(function () {
+      $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+      });
+    });
+</script>
+@stack('doc_end')
+</body>
+</html>
