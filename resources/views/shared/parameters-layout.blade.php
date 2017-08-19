f74ebd225c99cac218b24f9c0181becd4675a9e7
MohannadNaj <me@mohannad.io>
2017-08-19 13:05:04 +0300

    Cleaning ..

--- resources/views/shared/parameters-layout.blade.php
+++ resources/views/shared/parameters-layout.blade.php
@@ -4,7 +4,7 @@
     @include('parameters::shared.parameters-head')
 </head>
 <body>
-    <div id="app">
+    <div class="container-fluid" id="app">
       @include('parameters::shared.parameters-navbar')
     	@yield('content')
       <!-- Helpers -->
@@ -13,14 +13,5 @@
       <dropzone-upload ref="dropzone-modal" _target="parameters/addPhoto" _update_target="parameters/updatePhoto"></dropzone-upload>
     </div>
 <script src="{{asset('vendor/parameters/js/app.js') . '?v='.Illuminate\Support\Str::random()}}"></script>
-<script type="text/javascript">
-    @stack('js_footer')
-    $(document).ready(function () {
-      $('[data-toggle="offcanvas"]').click(function () {
-        $('.row-offcanvas').toggleClass('active')
-      });
-    });
-</script>
-@stack('doc_end')
 </body>
 </html>
