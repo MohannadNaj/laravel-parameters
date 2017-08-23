86f9c595fefe629ff79037c550eca4fc282d1ad2
MohannadNaj <me@mohannad.io>
2017-08-23 16:28:13 +0300

    Edit View

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,5 @@
 @extends('parameters::shared.parameters-layout')
 
-@php
-  $groupedParameters = $parameters->groupBy('category_id');
-@endphp
-
 @section('content')
   <parameters ref="all-parameters" :parameters_list='{!! $parameters->toJson(JSON_HEX_APOS) !!}'></parameters>
 @endsection
