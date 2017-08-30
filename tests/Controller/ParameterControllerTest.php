c4f56c2f9cf0792fc8e7f2979263d02e327a4a31
MohannadNaj <me@mohannad.io>
2017-08-30 03:28:39 +0300

    WIP

--- tests/Controller/ParameterControllerTest.php
+++ tests/Controller/ParameterControllerTest.php
@@ -12,13 +12,24 @@ use Illuminate\Support\Facades\Storage;
 class ParameterControllerTest extends ControllerTest
 {
 
-	public function test_1()
+	public function test_validation_on_create_parameter()
 	{
         $response = $this->actingAs(new User())->json('POST', '/parameters', ['name' => 'param','category_id' => 'e']);
         $response->seeStatusCode(422);
         $responseArray = $response->decodeResponseJson();
         $this->assertArrayContains(['type','label','category_id'], array_keys($responseArray));
 	}
+
+	public function test_validate_upload_file()
+    {
+        $response = $this->json('POST', '/parameters/addPhoto', [
+            'file' => 'aaa'
+        ]);
+        print_r($this->response->getStatusCode());
+
+//		print_r($responseArray);
+    }
+
 	public function test_upload_file()
     {
         Storage::fake('local');
@@ -26,6 +37,7 @@ class ParameterControllerTest extends ControllerTest
         $response = $this->json('POST', '/parameters/addPhoto', [
             'file' => UploadedFile::fake()->image('avatar.jpg')
         ]);
+
 		$responseArray = $response->decodeResponseJson();
 
         Storage::disk('local')->assertExists($responseArray['path']);
