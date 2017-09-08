7e7992bf51301db307364a25ce7b4d1d652ddd77
MohannadNaj <me@mohannad.io>
2017-09-08 12:45:06 +0300

    WIP

---
+++ specs/Modal.spec.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+
+import Modal from '../resources/assets/js/components/Modal';
+
+console.log(tst);
+
+describe('Modal', () => {
+
+  it('has a mounted hook', () => {
+    expect(typeof Modal.mounted).toBe('function')
+  })
+
+
+  it('sets the correct default data', () => {
+    expect(typeof Modal.data).toBe('function')
+    let defaultData = Modal.data()
+    expect(defaultData.data_save).toBe('')
+  })
+
+  it('correctly sets the message when mounted', () => {
+    let vm = new Vue(Modal).$mount()
+    vm.data_body = "heeey!";
+    expect(vm.data_body).toBe('heeey!')
+  })
+
+  it('renders the correct message', () => {
+    let Ctor = Vue.extend(Modal)
+    let vm = new Ctor({
+      propsData: {
+        title: 'Title!',
+        save: 'Save Message!',
+        html: '<p class="save-paragraph">Lorem!</p>'
+      }
+    }).$mount()
+
+    vm.$nextTick(() => {
+      expect(vm.$el.querySelector('.modal-title').textContent)
+      .toBe('Title!')
+      
+      expect(vm.$el.querySelector('.btn.btn-primary').textContent)
+      .toBe('Save Message!')
+
+      expect(vm.$el.querySelector('.save-paragraph').textContent)
+      .toBe('Lorem!')
+    })
+  })
+})
+
