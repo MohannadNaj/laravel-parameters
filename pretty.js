10d9737398b022fc09d5e524cb715b36facd28dc
MohannadNaj <me@mohannad.io>
2017-09-09 05:51:34 +0300

    using js-beautify

--- pretty.js
+++ pretty.js
@@ -16,7 +16,18 @@ const glob = require('glob')
 const prettier = require('prettier')
 const fs = require('fs')
 const parser = require('html-script-hook')
-const jsBeautify = require('js-beautify').js_beautify
+const jsBeautify = require('js-beautify').html
+const unformatted = [
+        // excluding `template` tag.
+        // https://github.com/beautify-web/js-beautify/blob/895a45d95b36d8e37a2b8ad4dda488c0e1a8f272/js/src/html/beautifier.js#L94
+        'a', 'abbr', 'area', 'audio', 'b', 'bdi', 'bdo', 'br', 'button', 'canvas', 'cite',
+        'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'iframe', 'img',
+        'input', 'ins', 'kbd', 'keygen', 'label', 'map', 'mark', 'math', 'meter', 'noscript',
+        'object', 'output', 'progress', 'q', 'ruby', 's', 'samp', /* 'script', */ 'select', 'small',
+        'span', 'strong', 'sub', 'sup', 'svg', 'textarea', 'time', 'u', 'var',
+        'video', 'wbr', 'text',
+        'acronym', 'address', 'big', 'dt', 'ins', 'strike', 'tt',
+    ];
 
 const files = process.argv[2] || 'resources/assets/js/**/*.{vue,js}'
 
@@ -26,7 +37,11 @@ glob(files, function(er, files) {
     let result
 
     if (file.endsWith('vue')) {
-      result = jsBeautify(source)
+      result = jsBeautify(source, {
+        unformatted,
+         indent_size: 2,
+        indent_scripts: "keep"
+      })
       result = parser(result, {
         scriptCallback: function(code) {
           return `\n\n${prettier.format(code, formatOptions)}\n`
